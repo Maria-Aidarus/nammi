c9435387d4967a53e2ad0e39c74ae352b3c53f65
Huda Joad <hjoad@andrew.cmu.edu>
2025-03-10 11:56:45 +0300

    set up everything except frontend i think

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # nammi
 
+
+run docker first! navigate to ur project root folder (ex. '~/projects/nammi')
+then run `docker-compose up --build`
+
+use `docker-compose down` to stop the containers
+
 <3
---
+++ app/auth.py
@@ -0,0 +1,18 @@
+from fastapi import Depends, HTTPException
+from datetime import datetime, timedelta
+import jwt
+from passlib.context import CryptContext
+
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
